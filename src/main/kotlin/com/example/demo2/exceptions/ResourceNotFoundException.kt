59611dab277b8d0da6c0fe62bed19924f9656d05
Marcos Vinicius <mvmarcos74@gmail.com>
2022-11-21 00:29:23 -0300

    update 3

--- src/main/kotlin/com/example/demo2/exceptions/ResourceNotFoundException.kt
+++ src/main/kotlin/com/example/demo2/exceptions/ResourceNotFoundException.kt
@@ -1,6 +1,7 @@
 package com.example.demo2.exceptions
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 import java.lang.*
-
-class UnsupportedMathOperationException(exception: String?) : RuntimeException(exception) {
-}
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class ResourceNotFoundException(exception: String?) : RuntimeException(exception)
